a319ff43fa95fe0ca5395b0366fb8cd5c389e16b
yichengdong <yichengdong@qq.com>
2023-11-03 17:11:06 +0800

    initial

---
+++ assets/Script/Airplane.ts
@@ -0,0 +1,38 @@
+// Learn TypeScript:
+//  - https://docs.cocos.com/creator/2.4/manual/en/scripting/typescript.html
+// Learn Attribute:
+//  - https://docs.cocos.com/creator/2.4/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - https://docs.cocos.com/creator/2.4/manual/en/scripting/life-cycle-callbacks.html
+
+const {ccclass, property} = cc._decorator;
+
+@ccclass
+export default class NewClass extends cc.Component {
+
+    @property(cc.Label)
+    label: cc.Label = null;
+
+    @property
+    text: string = 'hello';
+
+    // LIFE-CYCLE CALLBACKS:
+
+    // onLoad () {}
+
+    start () {
+
+    }
+
+    update (dt) {
+         //console.log('update');
+         var bgheight = 1280;
+         for(let bgNode of this.node.children){
+             bgNode.y -= 50 * dt;
+             if(bgNode.y < -bgheight){
+                 console.log('change position');
+                 bgNode.y+= bgheight*2;
+             }
+         }
+    }
+}
---
+++ assets/Script/PlayerScript.ts
@@ -0,0 +1,31 @@
+// Learn TypeScript:
+//  - https://docs.cocos.com/creator/2.4/manual/en/scripting/typescript.html
+// Learn Attribute:
+//  - https://docs.cocos.com/creator/2.4/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - https://docs.cocos.com/creator/2.4/manual/en/scripting/life-cycle-callbacks.html
+
+const {ccclass, property} = cc._decorator;
+
+@ccclass
+export default class PlayerScript extends cc.Component {
+
+    @property(cc.Label)
+    label: cc.Label = null;
+
+    @property
+    text: string = 'hello';
+
+    // LIFE-CYCLE CALLBACKS:
+
+    // onLoad () {}
+
+    start () {
+        var node = this.node;
+        node.on(cc.Node.EventType.MOUSE_DOWN, function (event) {
+        console.log('Mouse down');
+        }, this);
+    }
+
+    // update (dt) {}
+}

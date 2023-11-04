ccf06bf34d89cac38491cb1e4314bd9f56f5a326
yichengdong <1685368363@qq.com>
2023-11-04 15:58:21 +0800

    feat(Bird)背景的和地面的滚动

---
+++ assets/Script/Bird/BackgroundAndLandScript.ts
@@ -0,0 +1,31 @@
+
+const {ccclass, property} = cc._decorator;
+
+/**
+ * 背景和地面的脚本
+ */
+@ccclass
+export default class BackgroundAndLandScript extends cc.Component {
+
+    //速度
+    @property
+    speed: number = 4;
+
+    //背景宽度
+    @property
+    width: number = 288;
+
+    start () {
+
+    }
+
+    update(dt) {
+        //向左移动
+        for(let node of this.node.children){
+            node.x -= dt * this.speed;
+            if(node.x < this.width){
+                node.x += this.width * 2;
+            }
+        }
+    }
+}

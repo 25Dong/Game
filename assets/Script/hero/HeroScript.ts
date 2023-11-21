0f5eaae9f917b337192b997ad6d014a21a025db2
yichengdong <yichengdong@qq.com>
2023-11-21 18:22:37 +0800

    feat✨:  《Hero》连击

--- assets/Script/hero/HeroScript.ts
+++ assets/Script/hero/HeroScript.ts
@@ -4,8 +4,9 @@ const { ccclass, property } = cc._decorator;
 
 const Input = {}
 const State = {
-    stand: 1,
-    attack: 2
+    idle: 1,
+    attack: 2,
+    run: 3
 }
 /**
  * Hero
@@ -16,7 +17,7 @@ export default class Hero extends cc.Component {
     /**
      * 状态（默认为站立状态）
      */
-    private heroSate: number = State.stand;
+    private heroSate: number = State.idle;
     /**
      * 移动方向向量
      */
@@ -35,26 +36,43 @@ export default class Hero extends cc.Component {
      */
     private animationComponent: cc.Animation;
 
+    private attackProgress: number = -1;
+
     onLoad() {
         //绑定事件
         cc.systemEvent.on('keydown', this.onKeyDown, this);
         cc.systemEvent.on('keyup', this.onKeyUp, this);
 
         this.animationComponent = this.node.getComponent(cc.Animation);
+        this.animationComponent.on('stop', this.onAnimationStop, this);
     }
 
     onDestroy() {
         //解绑事件
         cc.systemEvent.off('keydown', this.onKeyDown, this);
         cc.systemEvent.off('keyup', this.onKeyUp, this);
+        this.animationComponent.off('stop', this.onAnimationStop, this);
     }
 
     onKeyDown(e) {
         Input[e.keyCode] = 1;
+        if (Input[cc.macro.KEY.q]) {
+            this.attackProgress = (this.attackProgress + 1) % 3;
+        }
+        // console.log("onKeyDown key:{}", e.keyCode)
     }
 
     onKeyUp(e) {
         Input[e.keyCode] = 0;
+
+        //console.log("onKeyUp key:{}", e.keyCode)
+    }
+
+    onAnimationStop() {
+        // console.log("onAnimationStop{}", this);
+        setTimeout(() => {
+            this.heroSate = State.idle;
+        }, 1000);
     }
 
     start() {
@@ -62,33 +80,50 @@ export default class Hero extends cc.Component {
     }
 
     update(dt) {
-        //播放的动画
-        let _anima: string;
-        
-        let scaleX = Math.abs(this.node.scaleX);
-        let lv = this.node.getComponent(cc.RigidBody).linearVelocity;
-        if (Input[cc.macro.KEY.a] || Input[cc.macro.KEY.left]) {
-            this.sp.x = -1;
-            this.node.scaleX = - scaleX;
-            _anima = 'run'
-        }
-        else if (Input[cc.macro.KEY.d] || Input[cc.macro.KEY.right]) {
-            this.sp.x = 1;
-            this.node.scaleX = scaleX;
-            _anima = 'run'
+        //维护状态
+        if (Input[cc.macro.KEY.q]) {
+            this.heroSate = State.attack;
+        } else if (Input[cc.macro.KEY.a] || Input[cc.macro.KEY.left]
+            || Input[cc.macro.KEY.d] || Input[cc.macro.KEY.right]) {
+            this.heroSate = State.run;
         } else {
-            this.sp.x = 0;
-            _anima = 'idle'
+            // this.heroSate = State.idle;
         }
-        if (this.sp.x) {
-            lv.x = this.sp.x * this.speed;
-        } else {
-            lv.x = 0;
-        }
-        this.node.getComponent(cc.RigidBody).linearVelocity = lv;
 
-        if (_anima) {
-            this.setAnimation(_anima);
+        //根据当前状态做出对应行为
+        if (this.heroSate == State.attack) {
+            //播放攻击动画
+            if (this.attackProgress == 0) {
+                this.setAnimation('attack1');
+            } else if (this.attackProgress == 1) {
+                this.setAnimation('attack2');
+            } else if (this.attackProgress == 2) {
+                this.setAnimation('attack3');
+            }
+            return;
+        }
+
+        if (this.heroSate == State.run) {
+            let scaleX = Math.abs(this.node.scaleX);
+            let lv = this.node.getComponent(cc.RigidBody).linearVelocity;
+            if (Input[cc.macro.KEY.a] || Input[cc.macro.KEY.left]) {
+                this.sp.x = -1;
+                this.node.scaleX = - scaleX;
+            }
+            else if (Input[cc.macro.KEY.d] || Input[cc.macro.KEY.right]) {
+                this.sp.x = 1;
+                this.node.scaleX = scaleX;
+            }
+            this.setAnimation('run');
+            lv.x = this.sp.x * this.speed;
+            this.node.getComponent(cc.RigidBody).linearVelocity = lv;
+            return
+        }
+
+        if (this.heroSate == State.idle) {
+            this.resetAttackProgress();
+            this.setAnimation('idle');
+            return;
         }
     }
 
@@ -99,5 +134,10 @@ export default class Hero extends cc.Component {
 
         this.curPlayerAnima = newAnimation;
         this.animationComponent.play(this.curPlayerAnima);
+        console.log("curPlayerAnima :{}", this.curPlayerAnima)
+    }
+
+    private resetAttackProgress() {
+        this.attackProgress = -1;
     }
 }
